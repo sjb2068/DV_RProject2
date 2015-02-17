49e522892906f77e21dc37eb6a3c938bdc2d8e47
Junbo Shim <junbo.shim.93@gmail.com>
2015-02-16 18:54:37 -0600

    project 2

---
+++ 01 Data/Dataframe.R
@@ -0,0 +1,1 @@
+df <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from realestate"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_js64948', PASS='orcl_js64948', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
---
+++ Rprofile.R
@@ -0,0 +1,7 @@
+library("plyr", lib.loc="~/R/win-library/3.1")
+library("RCurl", lib.loc="~/R/win-library/3.1")
+library("ggplot2", lib.loc="~/R/win-library/3.1")
+library("gplots", lib.loc="~/R/win-library/3.1")
+library("grid", lib.loc="~/R/win-library/3.1")
+library("reshape2", lib.loc="~/R/win-library/3.1")
+
